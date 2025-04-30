f689c9d174d13cf4a4907c2f52c6578faa788546
Thandeka325 <marhwayiza325@gmail.com>
2025-04-30 12:17:54 +0000

    Task 0: Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module asynchronous a generator that yields 10 random numbers.
+Each number is yielded after a 1-second delay.
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Function yields a random float between 0 and 10 every second, 10 times.
+
+    Returns:
+        AsyncGenerator[float, None]: An async generator yielding random floats.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
