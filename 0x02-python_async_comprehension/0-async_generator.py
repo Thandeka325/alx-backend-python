545f6dd4d41c0ee734066e9f6532b5f139e0c005
Thandeka325 <marhwayiza325@gmail.com>
2025-04-30 13:56:51 +0000

    Task 0: Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Function yields a random float between 0 and 10 every second, 10 times.
 
